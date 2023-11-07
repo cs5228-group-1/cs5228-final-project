1aaf970c9901235dd21c632facc6e3d290529a80
helloimsc <gsiowchuen@gmail.com>
2023-11-07 19:02:51 +0800

    added title to plot

--- plot_cv.py
+++ plot_cv.py
@@ -13,7 +13,7 @@ SETTING_LISTS = [
 ]
 
 
-def main(output_path: Path = typer.Argument(..., exists=False, file_okay=True, writable=True, help="Output figure path", path_type=Path)):
+def main(output_path: Path = typer.Argument(..., exists=False, file_okay=True, writable=True, help="Output figure path", path_type=Path), model_name: str = typer.Argument(..., exists=False, help="Model Name")):
     cross_validations = {
         name: pd.read_csv(INPUT_DIR / f"{name}_cross_validation.csv")['val-rmse'].values
         for name in SETTING_LISTS
@@ -24,7 +24,8 @@ def main(output_path: Path = typer.Argument(..., exists=False, file_okay=True, w
 
     loss_mean = np.array([np.mean(v) for k,v in sorted(cross_validations.items())])
     loss_std = np.array([np.std(v) for k,v in sorted(cross_validations.items())])
-
+    
+    plt.title(f"{model_name}")
     plt.errorbar(sorted(cross_validations.keys()), loss_mean, yerr=loss_std)
 
     plt.xlabel("Setting")
