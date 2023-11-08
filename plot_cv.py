299718c807a04375872476ba44e084b33f444617
Dang-Khoa Le Tan <letan.dangkhoa@gmail.com>
2023-11-09 00:32:53 +0800

    change x-tick name

--- plot_cv.py
+++ plot_cv.py
@@ -7,9 +7,8 @@ import pandas as pd
 from path import Path
 import matplotlib.pyplot as plt
 
-SETTING_LISTS = [
-    f"setting_{idx}" for idx in range(1, 5)
-]
+
+SETTING_LISTS = range(1, 6)
 
 
 def main(
@@ -18,7 +17,7 @@ def main(
         model_name: str = typer.Argument(..., exists=False, help="Model Name")):
 
     cross_validations = {
-        name: pd.read_csv(input_dir / f"{name}_cross_validation.csv")['val-rmse'].values
+        f"V{name}": pd.read_csv(input_dir / f"setting_{name}_cross_validation.csv")['val-rmse'].values
         for name in SETTING_LISTS
     }
 
